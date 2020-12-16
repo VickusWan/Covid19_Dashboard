957bccc99defb481c072ffbb68e86e5399c35d5f
VickusWan <71041007+VickusWan@users.noreply.github.com>
2020-12-16 01:44:36 -0800

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -7,15 +7,66 @@ Created on Mon Dec 14 00:27:43 2020
 
 from tkinter import *
 import data_extract as de
+import matplotlib.pyplot as plt
 
 root = Tk()
 root.title('COVID19 - Dashboard')
 root.geometry("400x400")
+
+country_cases, country_deaths = de.get_covid_data()
+total_cases, total_deaths, total_recovered = de.get_totals()
+total_cases = "{:,}".format(total_cases)
+total_deaths = "{:,}".format(total_deaths)
+total_recovered = "{:,}".format(total_recovered)
+
+def graph():
+    
+    inputs = clicked.get()
+    
+    cases = country_cases.loc[inputs]
+    deaths = country_deaths.loc[inputs]
+    
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.suptitle('{country} COVID Cases'.format(country=inputs))
+    axes[0,0].set_title('Confirmed Cases')
+    axes[0,1].set_title('Confirmed Deaths')
+    axes[1,0].set_title('Confirmed Cases LOG SCALE')
+    axes[1,1].set_title('Confirmed Deaths LOG SCALE')
+    
+    cases.plot(ax=axes[0,0], color='green')
+    deaths.plot(ax=axes[0,1], color='red')
+    cases.plot(ax=axes[1,0], logy=True, color='green')
+    deaths.plot(ax=axes[1,1], logy = True, color='red')
+    
+    return
+
+
+label1 = Label(root, text='Global Corona Virus Total Cases')
+label1.pack()
+label2 = Label(root, text=str(total_cases))
+label2.pack()
+label3 = Label(root, text='Global Corona Virus Total Deaths')
+label3.pack()
+label4 = Label(root, text=str(total_deaths))
+label4.pack()
+label5 = Label(root, text='Global Corona Virus Recoveries')
+label5.pack()
+label6 = Label(root, text=str(total_recovered))
+label6.pack()
 
 clicked = StringVar()
 clicked.set("Select a country")
 country_list = de.get_countries()
 drop = OptionMenu(root, clicked, *country_list)
 drop.pack()
+
+
+# =============================================================================
+# e = Entry(root)
+# e.pack()
+# =============================================================================
+
+button = Button(root, text="Graph", command=graph)
+button.pack()
 
 root.mainloop()
--- data_extract.py
+++ data_extract.py
@@ -9,10 +9,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def get_countries():
+    country_list.remove('US')
+    country_list.remove('Canada')
+    country_list.insert(0, '----------------------------------')
+    country_list.insert(0,'Canada')
+    country_list.insert(0, 'US')
+    
     return country_list
 
 def get_totals():
-    return [total_cases, total_deaths, total_recovered]
+    return total_cases, total_deaths, total_recovered
+
+def get_covid_data():
+    return country_cases, country_deaths
 
 province_cases = pd.read_csv("JHU_COVID19_data/time_series_covid19_confirmed_global.csv").drop(['Lat', 'Long'], axis=1)
 province_deaths = pd.read_csv("JHU_COVID19_data/time_series_covid19_deaths_global.csv").drop(['Lat', 'Long'], axis=1)
@@ -31,15 +40,5 @@ total_cases = province_cases.iloc[:,-1:].sum().tolist()[0]
 total_deaths = province_deaths.iloc[:,-1:].sum().tolist()[0]
 total_recovered = province_recovered.iloc[:,-1:].sum().tolist()[0]
 
-inputs = "Canada" 
 
-case = country_cases.loc[inputs]
-deaths = country_deaths.loc[inputs]
-
-fig, axes = plt.subplots(nrows=2, ncols=2)
-
-case.plot(ax=axes[0,0], color='green')
-deaths.plot(ax=axes[0,1], color='red')
-case.plot(ax=axes[1,0], logy=True, color='green')
-deaths.plot(ax=axes[1,1], logy = True, color='red')
 
